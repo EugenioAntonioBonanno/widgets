536701840017b2b374bbb58c59731e887057cab0
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2020-12-01 22:52:04 +0800

    widgets completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Convert from './components/Convert';
 
-function App() {
+const items = [ 
+  {
+    title: 'What is React',
+    content: 'React runs front end show'
+  }, 
+  {
+    title: 'Why use React?',
+    content: 'Because everyone loves it'
+  }, 
+  {
+    title: 'How do you use React',
+    content: 'Scream instructions into your computer screen and watch the magic'
+  }
+]
+
+const options = [
+  {
+    label: 'The color red',
+    value: 'red'
+  },
+  {
+    label: 'The color green',
+    value:  'green'
+  },
+  {
+    label: 'The color blue',
+    value: 'blue'
+  }
+];
+
+export default function App () {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Translate />
     </div>
   );
-}
+};
 
-export default App;
